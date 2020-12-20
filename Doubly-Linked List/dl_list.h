f96051fbcf285ae47e62939598888aa11e184abb
Mahmoud-Ehab <52333211+Mahmoud-Ehab@users.noreply.github.com>
2020-12-20 14:59:15 +0200

    Add files via upload

---
+++ Doubly-Linked List/dl_list.h
@@ -0,0 +1,140 @@
+/*
+* Written by Mahmoud Ehab Abd-alsalam
+* December, 2020
+*/
+
+#include <cstdlib>
+
+using namespace std;
+	
+namespace dll
+{
+	
+	//Declare (Struct) the linked list node
+	typedef struct LL_Node {
+	
+		int entry;
+		LL_Node *next;
+		LL_Node *prev;
+		
+	} LL_Node;
+	
+	//Function to create and return a LL_Node pointer
+	LL_Node* createNode(int entry, LL_Node *n, LL_Node *p) {
+	
+		LL_Node *node = (LL_Node*) malloc(sizeof(LL_Node));
+		node->entry = entry;
+		node->next = n;
+		node->prev = p;
+		
+		return node;
+	}
+	
+	//Linked List Class
+	class LinkedList
+	{
+		private:
+			LL_Node *firstNode = NULL;
+			LL_Node *lastNode = NULL;
+			int size = 0;
+			
+		public:
+			//Add new element in the list
+			void add(int entry) {
+				//The case when its the first element
+				if (firstNode == NULL && lastNode == NULL) {
+				
+					LL_Node *newNode = createNode(entry, NULL, NULL);
+					firstNode = newNode;
+					lastNode = newNode;
+					
+					size++;
+					return;
+				}
+				
+				//Otherwise
+				LL_Node *newNode = createNode(entry, NULL, lastNode); //Create New Node
+				lastNode->next = newNode;	//Assign the new node as the next of the lastNode
+				lastNode = newNode;	//Make the lastNode pointer points to the new(last)Node
+				
+				size++;
+			}
+			
+			//Return the entery of an element in the list
+			LL_Node* get(int index) {
+				if (index >= size || index < 0) {
+					return NULL;
+				}
+				else if (index == 0) {
+					return firstNode;
+				}
+				else if (index == size - 1) {
+					return lastNode;
+				}
+				
+				//If the index is between 0 and size-1
+				LL_Node *node = firstNode;
+				for (int i = 0; i < index; i++) {
+					node = node->next;
+				}
+				return node;
+			}
+			
+			//Return the size of the list
+			int getSize() {
+				return size;
+			}
+			
+			//Remove an element from the list
+			void remove(int index) {
+				if (index >= size || index < 0) {
+					return;
+				}
+				else if (index == 0) {
+					firstNode = firstNode->next;
+					free(firstNode->prev);
+					firstNode->prev = NULL;
+					size--;
+					return;
+				}
+				else if (index == size - 1) {
+					lastNode = lastNode->prev;
+					free(lastNode->next);
+					lastNode->next = NULL;
+					size--;
+					return;
+				}
+				
+				//If the index is between 0 and size-1
+				LL_Node *temp = get(index);
+				temp->prev->next = temp->next;
+				temp->next->prev = temp->prev;
+				
+				size--;
+				free(temp);
+			}
+			
+			//insert a new element in the list
+			void insert(int index, int entry) {
+				LL_Node *sNode = get(index);
+				if (sNode == NULL) return;
+				
+				//Allocate new node in the ram
+				LL_Node *newNode = createNode(entry, sNode, sNode->prev);
+				
+				//If index == 0, the user is insering in the first index
+				if (sNode->prev == NULL)
+					firstNode = newNode;
+				//Otherwise
+				else
+					sNode->prev->next = newNode;
+				
+				//Reconnect the list
+				sNode->prev = newNode;
+				
+				//Increase the size
+				size++;
+			}
+	};
+}
+
---
+++ Doubly-Linked List/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "dl_list.h"
+
+using namespace std;
+using namespace dll;
+
+void traverse(LinkedList list) {
+	LL_Node *temp = list.get(0);
+	while (temp != NULL) {
+		cout << temp->entry << '\n';
+		temp = temp->next;
+	}
+}
+
+int main() {
+	LinkedList list;
+	
+	cout << "************************" << '\n';
+	cout << "Test_0:  adding elements" << '\n';
+	cout << "************************" << '\n';
+	list.add(1);
+	list.add(2);
+	list.add(3);
+	list.add(4);
+	list.add(5);
+	list.add(6);
+	cout << "size = " << list.getSize() << '\n';
+	traverse(list);
+	
+	cout << "************************" << '\n';
+	cout << "Test_1: removing first element" << '\n';
+	cout << "************************" << '\n';
+	list.remove(0);
+	traverse(list);
+	
+	cout << "************************" << '\n';
+	cout << "Test_2: removing element 3 out of 5 elements" << '\n';
+	cout << "************************" << '\n';
+	list.remove(2);
+	traverse(list);
+	
+	cout << "************************" << '\n';
+	cout << "Test_3: removing last element" << '\n';
+	cout << "************************" << '\n';
+	list.remove(list.getSize() - 1);
+	traverse(list);
+	
+	cout << "************************" << '\n';
+	cout << "Test_4: insert new element in the beginning" << '\n';
+	cout << "************************" << '\n';
+	list.insert(0, 1);
+	traverse(list);
+	
+	cout << "************************" << '\n';
+	cout << "Test_5: insert new element in the middle" << '\n';
+	cout << "************************" << '\n';
+	list.insert(3, 4);
+	traverse(list);
+	
+	cout << "************************" << '\n';
+	cout << "Test_6: insert new element in the end" << '\n';
+	cout << "************************" << '\n';
+	list.insert(list.getSize()-1, 6);
+	traverse(list);
+	
+	return 0;
+}
